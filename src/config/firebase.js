9f2a00051f0d64adb8d3ae30ca53b4617415a83a
Talha Waleed <161113587+vcwaleed@users.noreply.github.com>
2025-01-30 07:35:41 -0800

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDNJUdGi3wMZyKcB7D8LItrTfnAKy2dhiQ",
-    authDomain: "next-firebase-39cf4.firebaseapp.com",
-    projectId: "next-firebase-39cf4",
-    storageBucket: "next-firebase-39cf4.firebasestorage.app",
-    messagingSenderId: "1018495510302",
-    appId: "1:1018495510302:web:3e6f7de354fd2a19a40b0f"
+/// your config data
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
